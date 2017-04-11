b0519560a285ef5ffac6626274736dd4d8315b11
JJ <j.j.deutscher@gmail.com>
2017-04-11 14:51:39 +0200

    Firebase config added voor ACC and PROD

--- src/environments/environment.acc.ts
+++ src/environments/environment.acc.ts
@@ -5,5 +5,13 @@ export const environment = {
   },
   vapid: {
     public_key: 'BAqgv_1uC3ZDDOzDgYAwT26nn_UvT4l4XAQTCe-bX_PHEJF5YMlymuxowv1HpYymsH_brYu8E8zSTXWOj4atB5s'
+  },
+  firebase_config: {
+    apiKey: "AIzaSyC3buFziZAhZNpkWIoSGjl8DjTbeENoyAY",
+    authDomain: "pwa-test-acc.firebaseapp.com",
+    databaseURL: "https://pwa-test-acc.firebaseio.com",
+    projectId: "pwa-test-acc",
+    storageBucket: "pwa-test-acc.appspot.com",
+    messagingSenderId: "791515343289"
   }
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,5 +5,13 @@ export const environment = {
   },
   vapid: {
     public_key: 'BAqgv_1uC3ZDDOzDgYAwT26nn_UvT4l4XAQTCe-bX_PHEJF5YMlymuxowv1HpYymsH_brYu8E8zSTXWOj4atB5s'
+  },
+  firebase_config: {
+    apiKey: "AIzaSyC0d5s23yKgP3c-5zzAE2FRA1vpa_RHJf8",
+    authDomain: "pwa-test-prod.firebaseapp.com",
+    databaseURL: "https://pwa-test-prod.firebaseio.com",
+    projectId: "pwa-test-prod",
+    storageBucket: "pwa-test-prod.appspot.com",
+    messagingSenderId: "86325808204"
   }
 };
